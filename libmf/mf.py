11227f20ddfad869e254fd4326b91d43ee07933f
tianhaowei <tianhaowei@yit.com>
2017-05-02 15:18:02 +0800

    python3 support

--- libmf/mf.py
+++ libmf/mf.py
@@ -73,7 +73,7 @@ class MF(object):
         self._options = MFParam()
         for kw in kwargs:
             if kw not in [i[0] for i in get_default_options()]:
-                print "Unrecognized keyword argument '{0}={1}'".format(kw, kwargs[kw])
+                print("Unrecognized keyword argument '{0}={1}'".format(kw, kwargs[kw]))
 
         for item in get_default_options():
             if item[0] not in kwargs:
--- mf_tests.py
+++ mf_tests.py
@@ -3,29 +3,29 @@ from libmf import mf
 
 test_data = mf.__generate_test_data(9000, 1000, 25000)
 
-print "testing fit method"
+print("testing fit method")
 mf_engine = mf.MF()
 try:
 	mf_engine.mf_fit(test_data)
-	print "OK"
+	print("OK")
 except Exception as e:
-	print e
-	print "FAILED"
+	print(e)
+	print("FAILED")
 
-print "testing predict method"
+print("testing predict method")
 try:
 	pred_data = mf.__generate_test_data(9000, 1000, 1000, indices_only=True)
-	print pred_data.shape
+	print(pred_data.shape)
 	mf_engine.mf_predict(pred_data)
-	print "OK"
+	print("OK")
 except Exception as e:
-	print "FAILED"
-	print e
+	print("FAILED")
+	print(e)
 
-print "testing cross validation"
+print("testing cross validation")
 try:
 	mf_engine.mf_cross_validation(test_data)
-	print "OK"
+	print("OK")
 except Exception as e:
-	print "FAILED"
-	print e
+	print("FAILED")
+	print(e)
